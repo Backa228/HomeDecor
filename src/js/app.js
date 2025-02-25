c0fcdddcdf3cd4bd7e057f5d503d3c8c8a297f3d
Backa228 <andrey.scott01@gmail.com>
2025-02-25 17:35:19 +0200

    25.02
    додали Брейк-поїнти

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,14 @@ console.log("Hello!!!")
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const menuToggle = document.querySelector('.sideMenu_icon');
+const sideMenu = document.querySelector('.header_nav');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    menuToggle.addEventListener('click', () => {
+        sideMenu.classList.toggle('active_sideMenu');
+        menuToggle.classList.toggle('open');
+    });
+});
