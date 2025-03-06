fe5c49891e808f4df5926444dbc3d4d849ff8e23
Backa228 <andrey.scott01@gmail.com>
2025-03-06 18:35:05 +0200

    lesson 06.03
    Розділили style.scss по файлам, зробили розмітку секції herо, розпочали верстати її.

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,31 @@ flsFunctions.isWebp();
 
 const menuToggle = document.querySelector('.sideMenu__icon');
 const sideMenu = document.querySelector('.header__nav');
-
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
 
 document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
         sideMenu.classList.toggle('active_sideMenu');
         menuToggle.classList.toggle('open');
+        document.classList.toggle('no-scroll');
+    });
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 850) {
+            sideMenu.classList.add('no-transition');
+            sideMenu.classList.remove('active_sideMenu');
+            menuToggle.classList.remove('open');
+        } else {
+            sideMenu.classList.remove('no-transition');
+        }
+    });
+    window.addEventListener('scroll', () => {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
+            header.classList.add('hidden');
+        } else {
+            header.classList.remove('hidden');
+        } 
+        lastScrollTop = scrollTop;
     });
 });
